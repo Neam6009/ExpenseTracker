e0382ce8d74dae6e77e7cc5b098980787b4212c8
Neam6009 <neeleshnani99@gmail.com>
2023-09-23 01:43:24 +0530

    git init of expense tracker

---
+++ src/components/Expenses.js
@@ -0,0 +1,17 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+
+const Expenses = (props)=>{
+    return (
+    <Card className='expenses'>
+        <ExpenseItem expense = {props.expenses[0]} />
+        <ExpenseItem expense = {props.expenses[1]} />
+        <ExpenseItem expense = {props.expenses[2]} />
+        <ExpenseItem expense = {props.expenses[3]} />
+    </Card>
+    )
+
+}
+
+export default Expenses;
